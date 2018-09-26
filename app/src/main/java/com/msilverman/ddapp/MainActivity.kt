d3397b686d4fb7a91043757a3e8da987f9cdd5a8
Matthew Silverman <msilverman@FLL1002096L.chewy.local>
2018-09-26 10:43:22 -0400

    Clean working state

--- app/src/main/java/com/msilverman/ddapp/MainActivity.kt
+++ app/src/main/java/com/msilverman/ddapp/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.main_activity2)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
